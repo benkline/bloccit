a4ebeebc3a235f23e072500b7040db303b0fcabd
benkline <ben.kline@gmail.com>
2016-02-10 20:44:05 -0700

    Seed db with ads

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,14 @@
 require "random_data"
 
 50.times do
+  Advertisement.create!(
+    title: RandomData.random_sentence,
+    copy: RandomData.random_paragraph,
+    price: 5
+  )
+end
+
+50.times do
   Post.create!(
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
@@ -16,5 +24,6 @@ posts = Post.all
 end
 
 puts "Seed finished"
+puts "#{Advertisement.count} ads created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
