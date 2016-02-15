04754982df461d9d21616bd2c8d8670a3e4fdc92
benkline <ben.kline@gmail.com>
2016-02-15 15:21:29 -0700

    halfway through... just checking in

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,13 +7,18 @@ RSpec.describe Post, type: :model do
   let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
 
   it { is_expected.to belong_to(:topic) }
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:body) }
+  it { is_expected.to validate_presence_of(:topic) }
+  it { is_expected.to validate_length_of(:title).is_at_least(5) }
+  it { is_expected.to validate_length_of(:body).is_at_least(20) }
 
-    describe "attributes" do
-      it "responds to title" do
-        expect(post).to respond_to(:title)
-      end
-      it "responds to body" do
-        expect(post).to respond_to(:body)
-      end
+  describe "attributes" do
+    it "responds to title" do
+      expect(post).to respond_to(:title)
+    end
+    it "responds to body" do
+      expect(post).to respond_to(:body)
     end
   end
+end
