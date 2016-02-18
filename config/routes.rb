539ad21db8c9353ec80e568abb51b8cb4a860981
benkline <ben.kline@gmail.com>
2016-02-18 16:28:36 -0700

    Add controller for user sign in / session control

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-
   resources :topics do
     resources :posts, except: [:index]
   end
 
   resources :users, only: [:new, :create]
+
+  resources :sessions, only:[:new, :create, :destroy]
 
   get 'about' => 'welcome#about'
 
