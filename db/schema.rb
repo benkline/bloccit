2cfb6315fba9a23345422770c1561be453f22dc1
ben kline <ben.kline@gmail.com>
2016-02-19 12:39:33 -0700

    new dev env

--- db/schema.rb
+++ db/schema.rb
@@ -13,15 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20160217173203) do
 
-  create_table "answers", force: :cascade do |t|
-    t.text     "body"
-    t.integer  "question_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  add_index "answers", ["question_id"], name: "index_answers_on_question_id"
-
   create_table "comments", force: :cascade do |t|
     t.text     "body"
     t.integer  "post_id"
@@ -40,25 +31,6 @@ ActiveRecord::Schema.define(version: 20160217173203) do
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
-
-  create_table "questions", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.boolean  "resolved"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "sponsored_posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.integer  "price"
-    t.integer  "topic_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "sponsored_posts", ["topic_id"], name: "index_sponsored_posts_on_topic_id"
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
