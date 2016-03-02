b5461c42bac0855a796c4df5af4dc866d8576088
benkline <ben.kline@gmail.com>
2016-03-01 21:13:56 -0700

    Add MVC for Voting

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160224153920) do
+ActiveRecord::Schema.define(version: 20160302035554) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "body"
@@ -21,6 +21,17 @@ ActiveRecord::Schema.define(version: 20160224153920) do
   end
 
   add_index "answers", ["question_id"], name: "index_answers_on_question_id"
+
+  create_table "commenters", force: :cascade do |t|
+    t.integer  "comment_id"
+    t.integer  "commentable_id"
+    t.string   "commentable_type"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
+
+  add_index "commenters", ["comment_id"], name: "index_commenters_on_comment_id"
+  add_index "commenters", ["commentable_type", "commentable_id"], name: "index_commenters_on_commentable_type_and_commentable_id"
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -57,6 +68,7 @@ ActiveRecord::Schema.define(version: 20160224153920) do
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
     t.integer  "user_id"
+    t.float    "rank"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
@@ -97,5 +109,16 @@ ActiveRecord::Schema.define(version: 20160224153920) do
     t.datetime "updated_at",      null: false
     t.integer  "role"
   end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "value"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "votes", ["post_id"], name: "index_votes_on_post_id"
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id"
 
 end
