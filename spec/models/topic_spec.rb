f6591c3a4799fe8714fb5cf3ad89476b9c389bc6
benkline <ben.kline@gmail.com>
2016-03-07 11:05:20 -0700

    Add scope for private topics

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -5,12 +5,10 @@ RSpec.describe Topic, type: :model do
 
   let(:topic) { create(:topic) }
 
-# check model relationshps
   it { is_expected.to have_many(:posts) }
   it { is_expected.to have_many(:labelings) }
   it { is_expected.to have_many(:labels).through(:labelings) }
 
-# check response to parameters
   describe "attributes" do
     it "responds to name" do
       expect(topic).to respond_to(:name)
@@ -28,5 +26,22 @@ RSpec.describe Topic, type: :model do
       expect(topic.public).to be(true)
     end
   end
-# end check response to parameters
+
+  describe "scopes" do
+    before do
+      @public_topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)
+      @private_topic = Topic.create!(name:  RandomData.random_sentence, description:  RandomData.random_paragraph, public: false)
+    end
+
+   describe "visible_to(user)" do
+      it "returns all topics if the user is present" do
+        user = User.new
+        expect(Topic.visible_to(user)).to eq(Topic.all)
+      end
+
+      it "returns only public topics if user is nil" do
+        expect(Topic.visible_to(nil)).to  eq([@public_topic])
+      end
+    end
+  end
 end
