3d95572a9e774b5302f290dbd0aa95d042255a17
benkline <ben.kline@gmail.com>
2016-03-14 21:37:32 -0600

    completed third part of assignment - thanks again Richard

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :favorite_posts, through: :favorites, source: :post
+
 
   before_save { self.email = email.downcase }
   before_save { self.role ||= :member }
